1b906edb4d435a25d3f7988f3207ee108352c073
GabrielNeja <gsneja@minha.fag.edu.br>
2023-11-14 22:29:24 -0300

    Incluindo projeto no repositório

---
+++ spring/src/main/java/com/jogo/spring/JogoDaVelhaApplication.java
@@ -0,0 +1,15 @@
+package com.jogo.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogoDaVelhaApplication {
+	public static void main(String[] args) {                                        
+		SpringApplication.run(JogoDaVelhaApplication.class, args);    
+	}
+}
+	
+		
+
+
---
+++ spring/src/main/java/com/jogo/spring/controller/GameController.java
@@ -0,0 +1,17 @@
+package com.jogo.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/jogo")
+@CrossOrigin("*")                        
+public class GameController {
+
+    @GetMapping              
+    public String home() {
+        return null;
+    }
+}
